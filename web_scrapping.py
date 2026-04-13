c2f604ea1eae1e48804de42f9945d8d8e30a48b5
clmateus <cl.mateuscosta@gmail.com>
2026-04-13 17:54:56 -0300

    web scrapping

---
+++ web_scrapping.py
@@ -0,0 +1,31 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import WebBaseLoader
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.environ.get('api_key')
+os.environ['GROQ_API_KEY'] = api_key
+
+loader = WebBaseLoader('https://www.scrapethissite.com/pages/')
+chat = ChatGroq(model='llama-3.3-70b-versatile')
+
+lista_documentos = loader.load()
+documento = ''
+
+for doc in lista_documentos:
+    documento += doc.page_content
+
+template = ChatPromptTemplate.from_messages(
+    [
+        ('system', 'Você é um assistente amigável chamado Márcio Bot e tem acesso as seguintes informações para dar as suas respostas: {documentos_informados}'),
+        ('user', '{input}')        
+    ]
+)
+
+chain = template | chat
+
+resposta = chain.invoke({'documentos_informados': documento, 'input': 'Quais os conteúdos do site?'})
+
+print(resposta.content)
