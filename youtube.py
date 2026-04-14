a5a6e56e64ea86b8e97c9a8684d979841b5d2ec2
clmateus <cl.mateuscosta@gmail.com>
2026-04-14 10:01:35 -0300

    Youtube IA reader

---
+++ youtube.py
@@ -0,0 +1,27 @@
+from langchain_community.document_loaders.parsers import language
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import YoutubeLoader
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.environ.get('api_key')
+os.environ['GROQ_API_KEY'] = api_key
+
+chat = ChatGroq(model='llama-3.3-70b-versatile')
+
+url = 'https://www.youtube.com/watch?v=tArMc9-s7Fw'
+loader = YoutubeLoader.from_youtube_url(url)
+
+lista_documentos = loader.load()
+documento = ''
+
+template = ChatPromptTemplate.from_messages([
+    ('system', 'Você é um assistente amigável que possui as seguintes informações para formular uma resposta: {informacoes}'),
+    ('user', '{input}')
+])
+
+chain_youtube = template | chat
+resposta = chain_youtube.invoke({'informacoes': documento, 'input': 'Segundo o autor do documento, há quanto tempo ele programa em Python?'})
+print(resposta.content)
